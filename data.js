1fd8b37ef793630266c918ce98234d73c2b45e50
Tim Jackson <timjacksonm@gmail.com>
2021-01-18 16:46:41 -0600

    added reset button and functionality

--- data.js
+++ data.js
@@ -1,6 +1,9 @@
-    const container = document.querySelector('#container');
+function createGrid() {
+   const container = document.querySelector('#container');
+   let i = 0;
+   let input = 16
 
-   for(let i=0; i < 16; i++) {
+   for(let i=0; i < input; i++) {
        container.appendChild(document.createElement('div'));
    }
 
@@ -9,7 +12,33 @@
    content.forEach((div) => {
    div.setAttribute('class', 'gridSquares');
    div.addEventListener('mousemove', function colorDivGrid() {
-       div.setAttribute('class', 'color');
-   } )
-   }
-);
+       div.setAttribute('class', 'color gridSquares');
+                }
+            )
+        }
+    )
+};
+function resetButton() {
+    const body = document.querySelector('#body');
+
+    const createButton = document.createElement('button');
+    body.appendChild(createButton);
+    createButton.classList.add('buttonStyles');
+    createButton.setAttribute('id', 'resetButton');
+    createButton.setAttribute('onClick', 'removeClass()')
+    createButton.textContent = 'Reset';
+};
+function removeClass() {
+    const content = container.querySelectorAll('div');
+
+    content.forEach((div) => {
+    div.classList.remove('color');
+        }
+    )
+};
+
+resetButton();
+createGrid();
+
+
+
