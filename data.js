9c52e047e1d6549d29c2f3795e6c535fcc6f303f
Tim Jackson <timjacksonm@gmail.com>
2021-01-18 14:38:02 -0600

    added 16x16 grid to center of page.

---
+++ data.js
@@ -0,0 +1,13 @@
+    const container = document.querySelector('#container');
+
+   for(let i=0; i < 16; i++) {
+       container.appendChild(document.createElement('div'));
+   }
+
+   const content = container.querySelectorAll('div');
+
+   content.forEach((div) => {
+   div.setAttribute('class', 'gridSquares')
+   }
+);
+
